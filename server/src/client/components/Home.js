f990c16c56182e0b5932a354e3c5eeb605afbfdf
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-26 11:01:01 +0200

    add callback

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Im home component</div>;
+  return (
+    <div>
+      <div>Im home component</div>
+      <button onClick={() => console.log('hey')}>Click me</button>
+    </div>
+  );
 };
 
 export default Home;
